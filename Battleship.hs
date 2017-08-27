ff239c49532eaca1a59aca63de3fb3ba45875bb1
Renhao Tan <u6211458@n113lt08.csitlabs>
2017-08-27 20:08:59 +1000

    Battleship.hs updated

--- Battleship.hs
+++ Battleship.hs
@@ -3,7 +3,7 @@
 -- permission for the usage of their work by the cohort
 -- enrolled in the course COMP1100 in 2017 Semester 2 for
 -- education purposes only. No commercial usage is allowed
--- without the explicit permission from the original author. 
+-- without the explicit permission from the original author.
 --
 -- Assignment completed by:
 -- Name    : Renhao Tan
@@ -24,13 +24,16 @@ showBoard :: Board -> String
 showBoard cells = "+" ++ replicate 21 '-' ++ "+\n"
                     ++ intercalate "\n" (map rowsToGrid cells)
                     ++ "\n+" ++ replicate 21 '-' ++ "+"
-    where          
+    where
         rowsToGrid :: Row Cell -> String
         rowsToGrid row = "| " ++ intercalate " " (map cStateToCell row) ++ " |"
-        
+
         cStateToCell :: Cell -> String
-        cStateToCell = undefined
-    
+        cStateToCell c = case c of
+            Unchecked -> " "
+            Hit        -> "o"
+            Miss       -> "x"
+
 data Cell = Unchecked | Hit | Miss
     deriving (Show, Read)
 type Matrix a  = [Row a]
@@ -54,29 +57,34 @@ data State = State {board        :: Board,
                     condition    :: Condition,
                     numMoves     :: Integer}
                     deriving (Show)
-                    
+
 type ShipsOnGrid = [ShipType]
 data GenShips = GenShips {gsShips       :: Ships,
                           existingShips :: ShipsOnGrid,
                           finished      :: Bool}
                           deriving (Show)
-                    
+
 -- updateList replaces an element by index in a given list.
 updateList :: [a] -> Int -> a -> [a]
 updateList list n x = take (n) list ++ [x] ++ drop (n + 1) list
 
 validPlacement :: GenShips -> Coordinate -> Direction -> ShipType -> Bool
-validPlacement gs c d s = 
+validPlacement gs c d s =
                     not (s `elem` (existingShips gs))
                 && (all coordInBound onCoords)
                 && (not $ any (\x -> isShipAtCoord x (gsShips gs)) $
                          filter coordInBound $ nub $ concatMap getNeighbours onCoords)
     where
         onCoords = getCoords c d (shipLength s)
-        
+
 shipLength :: ShipType -> Integer
-shipLength = undefined
-    
+shipLength s = case s of
+    Carrier    -> 5
+    Battleship -> 4
+    Cruiser    -> 3
+    Submarine  -> 3
+    Destroyer  -> 2
+
 -- getCoords returns the list of Coordinate which would be
 -- occupied by the ship.
 getCoords :: Coordinate -> Direction -> Integer -> [Coordinate]
@@ -85,14 +93,16 @@ getCoords (i, j) dir l = case dir of
     Right -> map (\x -> (x, j)) [i .. (i + l - 1)]
     Up    -> map (\x -> (i, x)) [j, (j - 1) .. (j - l + 1)]
     Left  -> map (\x -> (x, j)) [i, (i - 1) .. (i - l + 1)]
-    
+
 -- getNeighbours returns a 9-element list containing
 -- coordinates around the given coordinate and itself.
 getNeighbours :: Coordinate -> [Coordinate]
 getNeighbours (x, y) = [(i, j) | i <- [(x - 1) .. (x + 1)], j <- [(y - 1) .. (y + 1)]]
-    
+
 coordInBound :: Coordinate -> Bool
-coordInBound = undefined
+coordInBound (x, y)
+    | (x >= 0 && x <= 9) && (y >= 0 && y <= 9) = True
+    | otherwise                                = False
 
 -- isShipAtCoord determines if there is a ship already placed
 -- at the coord.
@@ -100,10 +110,36 @@ isShipAtCoord :: Coordinate -> Ships -> Bool
 isShipAtCoord (x, y) grid = grid !! (fromIntegral y) !! (fromIntegral x)
 
 placeShip :: GenShips -> Coordinate -> Direction -> ShipType -> GenShips
-placeShip = undefined
+placeShip gs (x, y) dir st
+    | length (existingShips gs) == 5  = GenShips (gsShips gs) (existingShips gs) True
+    | validPlacement gs (x, y) dir st = placeShip (GenShips (gsShips gs) (updateList (existingShips gs) (length (existingShips gs)) st) (finished gs)) (x, y) dir st
+    | otherwise                       = placeShip gs (x, y) dir st
 
 transitionState :: State -> Coordinate -> State
-transitionState = undefined
+transitionState state (x, y)
+    | judgeCondition (condition state) = state
+    | (numMoves state) == 20 = State (board state) (ships state) Lost 20
+    | (x < 0 || x > 9) || (y < 0 || y > 9) = state
+    | ((ships state) !! (fromIntegral y)) !! (fromIntegral x) && length (countHit (board state) 0) == 4 = State (updateList (board state) (fromIntegral y) (updateList ((board state) !! (fromIntegral y)) (fromIntegral x) Hit)) (ships state) Won (numMoves state)
+    | ((ships state) !! (fromIntegral y)) !! (fromIntegral x) = State (updateList (board state) (fromIntegral y) (updateList ((board state) !! (fromIntegral y)) (fromIntegral x) Hit)) (ships state) (condition state) (numMoves state)
+    | not (((ships state) !! (fromIntegral y)) !! (fromIntegral x)) = oneMoreMove (State (updateList (board state) (fromIntegral y) (updateList ((board state) !! (fromIntegral y)) (fromIntegral x) Miss)) (ships state) (condition state) (numMoves state))
+
+judgeCondition :: Condition -> Bool
+judgeCondition con = case con of
+    Won     -> True
+    Lost    -> True
+    Playing -> False
+
+countHit :: Board -> Int -> [Cell]
+countHit b n
+    | n == 9 = filter judgeHit (b !! n)
+    | n >= 0 && n < 9 = filter judgeHit (b !! n) ++ countHit b (n + 1)
+
+judgeHit :: Cell -> Bool
+judgeHit c = case c of
+    Hit -> True
+    Miss -> False
+    Unchecked -> False
 
 oneMoreMove :: State -> State
 oneMoreMove (State b s c m) = State b s c (m + 1)
